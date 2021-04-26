a5ceea67ccde66847be6d64e38f64c63ad209e63
dmb225 <daomalick225@gmail.com>
2021-04-26 12:21:31 +0200

    Add roman_to_int

---
+++ source/strings/roman_to_int.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+class NotEnoughItemsError(Exception):
+    pass
+
+
+def roman_to_int(s: str) -> int:
+    """
+    :param s: A roman form number
+    :return: The corresponding integer
+    """
+    if len(s) < 1:
+        raise NotEnoughItemsError('The roman number should have at least 1 letter')
+        return -1
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    i, res = 0, 0
+    while i < len(s):
+        if i < len(s) - 1 and d[s[i]] < d[s[i + 1]]:
+            res += d[s[i + 1]] - d[s[i]]
+            i += 2
+        else:
+            res += d[s[i]]
+            i += 1
+    return res
+
+
+@pytest.mark.parametrize('roman, res', [
+    ('III', 3),
+    ('LVIII', 58),
+    ('MCMXCIV', 1994),
+])
+def test_roman_to_int(roman, res):
+    assert roman_to_int(roman) == res
