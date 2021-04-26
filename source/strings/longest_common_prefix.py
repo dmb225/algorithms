aa3ccece4e97e4ac08f18a20e66fce7956b0fdb9
dmb225 <daomalick225@gmail.com>
2021-04-26 17:31:01 +0200

    add longest_common_prefix

---
+++ source/strings/longest_common_prefix.py
@@ -0,0 +1,31 @@
+import pytest
+from typing import List
+
+
+def common_prefix(s1: str, s2: str) -> str:
+    cp = ''
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            return cp
+        cp += c1
+    return cp
+
+
+def longest_common_prefix(strings: List[str]) -> str:
+    """
+    :param strings: a list of strings
+    :return: the longest common prefix of these strings
+    """
+    lcp = strings[0]
+    for i in range(1, len(strings)):
+        cp = common_prefix(strings[i - 1], strings[i])
+        lcp = min(lcp, cp)
+    return lcp
+
+
+@pytest.mark.parametrize('strings, res', [
+    (['flower', 'flow', 'flight'], 'fl'),
+    (['dog', 'racecar', 'car'], ''),
+])
+def test_longest_common_prefix(strings, res):
+    assert longest_common_prefix(strings) == res
